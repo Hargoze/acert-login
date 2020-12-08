4ec6e79316451c858e37395d8603a6e189b7f315
Hargoze <francois.lecarpentier99@gmail.com>
2020-12-08 16:48:47 +0100

    improve responsive

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Head from "next/head"
 import { useRouter } from 'next/router'
 import { Box, Stack, Flex, Text, Input, InputGroup, Icon, InputRightElement, Button, Checkbox, Link, Image } from "@chakra-ui/react"
-import { Container } from '../components/Container'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 
 const Post = () => {
@@ -10,22 +9,29 @@ const Post = () => {
   const [show, setShow] = React.useState(false)
   const handleClick = () => setShow(!show)
   return (
-  <Container>
-  <Head>
-    <link rel="icon" sizes="48x48" href="/miniLogo.ico"/>
-    <meta charSet="utf-8" />
-    <meta name="description" content="page description"/>
-    <title>acert | accedez à votre compte</title>
-  </Head>
-  <Flex w="100%">
-    <Link href="/">
-      <Image ml="6" src="/SodaLogo300-120.png" alt="Acert Logo" width="225px" height="90px"/>
-    </Link>
-  </Flex>
+  <Flex 
+  direction="column"
+  alignItems="center"
+  justifyContent="flex-start"
+  width="100%"
+  bgImage="url('/apiagro-background.png')" bgRepeat="no-repeat" bgSize="contain"
+  bg={{base:"white", lg:"none"}}
+  >
+    <Head>
+      <link rel="icon" sizes="48x48" href="/miniLogo.ico"/>
+      <meta charSet="utf-8" />
+      <meta name="description" content="page description"/>
+      <title>acert | accedez à votre compte</title>
+    </Head>
+    <Flex w="100%">
+      <Link href="/">
+        <Image ml="6" src="/SodaLogo300-120.png" alt="Acert Logo" width="225px" height="90px"/>
+      </Link>
+    </Flex>
     <Stack
     w="full" h="930px"
-    bgImage="url('/apiagro-background.png')" bgRepeat="no-repeat" bgSize="contain"
-    align="center" justify="center" display={{base:"none", lg:"flex"}}>
+    align="center" justify="center" display={{base:"none", lg:"flex"}}
+    >
       <Flex rounded="lg" overflow="hidden" w={{default:"90%", lg:"50%"}}>
         <Stack align="center" justify="center" bg="blue.400" w="50%" h="420px">
           <Text color="white" px="10" fontSize="26px">Bienvenue sur la plateforme d’échanges de données utiles pour le secteur agricole</Text>
@@ -34,7 +40,16 @@ const Post = () => {
           <Text>adresse email professionnelle</Text>
           <Input placeholder="Email pro"/>
           <Text>mot de passe</Text>
-          <Input placeholder="Mot de passe" type="password"/>
+          <InputGroup size="md">
+          <Input
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Entrez votre mot de passe"
+          />
+          <InputRightElement width="4.5rem">
+            <Icon onClick={handleClick} as={show ? ViewIcon : ViewOffIcon}/>
+          </InputRightElement>
+        </InputGroup>
           <Button rounded="lg" colorScheme="primary">s'identifier</Button>
           <Checkbox>se souvenir de moi</Checkbox>
           <Text>Vous n'avez pas encore de compte ?</Text>
@@ -44,8 +59,8 @@ const Post = () => {
         </Stack>
       </Flex>
     </Stack>
-    <Stack display={{base:"flex", lg:"none"}}>
-      <Stack align="center" justify="center" w="100%" lineHeight="1.2" py="6">
+    <Stack display={{base:"flex", lg:"none"}} w={{base:"100%", md:"60%"}}>
+      <Stack align="center" justify="center"  lineHeight="1.2" py="6">
         <Text color="secondary.500" px="10" fontSize="26px">Bienvenue sur la plateforme d’échanges de données utiles pour le secteur agricole</Text>
       </Stack>
       <Stack align="start" justify="start" w="100%" bg="white" p="6" fontSize="18px">
@@ -70,7 +85,7 @@ const Post = () => {
         <Link color="blue.500">Réinitialiser</Link>
       </Stack>
     </Stack>
-  </Container>
+  </Flex>
   )
 }
 
